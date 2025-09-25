187948520bac5120274260c544ed7a7601431852
Aiver7 <aiverson.palacios@udea.edu.co>
2025-09-25 13:05:26 -0500

    Descompresión RLE

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -24,3 +24,28 @@ void desencriptar(const unsigned char* entrada, int longitud,
         salida[i] = rotarDerecha(temp, n);
     }
 }
+
+//  FUNCIONES RLE
+
+void descomprimirRLE(const unsigned char* comprimido, int longComp,
+                     unsigned char* salida, int& longSalida) {
+    longSalida = 0;
+    int i = 0;
+
+    while (i < longComp) {
+        int num = 0;
+        while (i < longComp && comprimido[i] >= '0' && comprimido[i] <= '9') {
+            num = num * 10 + (comprimido[i] - '0');
+            i++;
+        }
+
+        if (i >= longComp) break;
+
+        unsigned char c = comprimido[i];
+        i++;
+
+        for (int j = 0; j < num; j++) {
+            salida[longSalida++] = c;
+        }
+    }
+}
